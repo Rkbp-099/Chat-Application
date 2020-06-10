dedfb472ac33bc6332e6a71b48cc91b6db9e6799
Raktim Bijoypuri <bijoypuriraktim12@gmail.com>
2020-06-10 21:25:57 +0530

    priliminary socket connection work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+const http = require('http');
 const path = require('path');
 const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname,'/../public')));
 
@@ -8,10 +13,20 @@ app.get('/', function(req, res){
     res.render('index');    
 });
 
+let count = 0;
 
+io.on('connection', (socket) => {
+    console.log('New Websocket connection established');
+    socket.emit('countUpdated', count);
+
+    socket.on('increment!', () => {
+        count++;
+        io.emit('countUpdated', count);
+    })
+})
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
