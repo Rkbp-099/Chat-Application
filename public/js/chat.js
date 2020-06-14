44386c0b2956d8a0be23353dbf80cad34ef4fd53
Raktim Bijoypuri <bijoypuriraktim12@gmail.com>
2020-06-14 21:46:13 +0530

    Getting messages and url print to the screen and links opening in a new tab

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,21 +5,43 @@ const $messageForm = document.querySelector('#message-form');
 const $messageFormInput = $messageForm.querySelector('input');
 const $messageFormButton = $messageForm.querySelector('#submitButton');
 const $locationButton = document.querySelector('#send-location');
+const $messages = document.querySelector("#messages");
 
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML;
 
 socket.on('message', (message) => {
     console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        message,
+    });
+    const htmlObject = document.createElement('div');
+    htmlObject.innerHTML = html
+    $messages.insertAdjacentElement('beforeend', htmlObject);
+
+});
+
+socket.on('locationMessage', (url) => {
+    console.log(url);
+    const locationLink = Mustache.render(locationMessageTemplate, {
+        url,
+    });
+    const urlObject = document.createElement('div');
+    urlObject.innerHTML = locationLink;
+    $messages.insertAdjacentElement('beforeend', urlObject);
+    
 });
 
 
 $messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+
     $messageFormButton.setAttribute('disabled', 'disabled');
     const message = e.target.elements.message.value;
     // console.log(message);
     socket.emit('sendMessage', message, (error) => {
-        $messageFormButton.removeAttribute('disabled');   
+        $messageFormButton.removeAttribute('disabled');
         $messageFormInput.value = "";
         $messageFormInput.focus();
 
@@ -30,7 +52,7 @@ $messageForm.addEventListener('submit', (e) => {
     });
 });
 
-$locationButton.addEventListener('click',() => {
+$locationButton.addEventListener('click', () => {
     $locationButton.setAttribute("disabled", "disabled");
     if (!navigator.geolocation) {
         return alert("Does not Support Location Your Browser!!");
@@ -38,7 +60,7 @@ $locationButton.addEventListener('click',() => {
 
     navigator.geolocation.getCurrentPosition((position) => {
         // console.log(position.coords.latitude);
-        socket.emit('sendLocation',{
+        socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
         }, (error) => {
