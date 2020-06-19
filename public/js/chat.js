a399913c4ce8bcdc5193e153eb59fab5b6444390
Raktim Bijoypuri <bijoypuriraktim12@gmail.com>
2020-06-19 20:56:12 +0530

    autoscrolling added

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,14 +6,38 @@ const $messageFormInput = $messageForm.querySelector('input');
 const $messageFormButton = $messageForm.querySelector('#submitButton');
 const $locationButton = document.querySelector('#send-location');
 const $messages = document.querySelector("#messages");
+const $sidebar = document.querySelector("#sidebar");
 
 // Templates
 const messageTemplate = document.querySelector("#message-template").innerHTML;
 const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 
 // Options
 const { username, room } = Qs.parse(location.search, {ignoreQueryPrefix: true});
 
+const autoScroll = () => {
+    // New message content
+    const $newMessage = $messages.lastElementChild
+
+    // height of new message
+    const newMessageStyle = getComputedStyle($newMessage);
+    const newMessageMargin = parseInt(newMessageStyle.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+    // visible height
+    const visibleHeight = $messages.offsetHeight;
+
+    // container height
+    const containerHeight = $messages.scrollHeight;
+
+    // how far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight;
+
+    if (containerHeight - newMessageHeight <= scrollOffset){
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
 
 
 socket.on('message', (message) => {
@@ -25,8 +49,9 @@ socket.on('message', (message) => {
     });
     const htmlObject = document.createElement('div');
     htmlObject.innerHTML = html
+    htmlObject.setAttribute('class', 'message');
     $messages.insertAdjacentElement('beforeend', htmlObject);
-
+    autoScroll();
 });
 
 socket.on('locationMessage', (urlOb) => {
@@ -38,9 +63,21 @@ socket.on('locationMessage', (urlOb) => {
     });
     const urlObject = document.createElement('div');
     urlObject.innerHTML = locationLink;
+    htmlObject.setAttribute('class', 'message');
     $messages.insertAdjacentElement('beforeend', urlObject);
-    
+    autoScroll();
 });
+
+socket.on('roomData', ({ room, users}) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    const htmlOb = document.createElement('div');
+    htmlOb.innerHTML = html;
+    // $sidebar.insertAdjacentElement()
+    $sidebar.innerHTML = html;
+})
 
 
 $messageForm.addEventListener('submit', (e) => {
